0908782f88ea8416a9343d2f18cfd3b42f1fa5c0
Frederico Naso <nasofrederico@gmail.com>
2023-08-21 16:35:43 +0200

    login script v1

---
+++ FrontEnd/assets/login.js
@@ -0,0 +1,6 @@
+const loginForm = document.querySelector('form');
+const errorMessage = document.querySelector('#errorMessage');
+
+loginForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+})
